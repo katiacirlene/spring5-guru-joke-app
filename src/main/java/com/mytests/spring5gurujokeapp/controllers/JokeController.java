01066802a51f34b020f3035b0ce8301c9a3ab45d
Katia <katiacirlene@bitbucket.org>
2018-10-08 16:50:52 -0300

    creating controller.

---
+++ src/main/java/com/mytests/spring5gurujokeapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.mytests.spring5gurujokeapp.controllers;
+
+import com.mytests.spring5gurujokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+
+}
