6dd9896c0478d38e499cf9cc8218c2bd7e00a789
Katia <katiacirlene@bitbucket.org>
2018-10-07 21:10:08 -0300

    adding project generated with Spring Initializr.

---
+++ src/main/java/com/mytests/spring5gurujokeapp/Spring5GuruJokeAppApplication.java
@@ -0,0 +1,12 @@
+package com.mytests.spring5gurujokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5GuruJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5GuruJokeAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mytests/spring5gurujokeapp/Spring5GuruJokeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mytests.spring5gurujokeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5GuruJokeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
