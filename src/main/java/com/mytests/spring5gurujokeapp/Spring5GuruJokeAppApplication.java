d533b3dfd52e854c9e31e05741b03817df42b1fc
Katia <katiacirlene@bitbucket.org>
2018-10-09 18:00:39 -0300

    Spring XML configuration example.

--- src/main/java/com/mytests/spring5gurujokeapp/Spring5GuruJokeAppApplication.java
+++ src/main/java/com/mytests/spring5gurujokeapp/Spring5GuruJokeAppApplication.java
@@ -2,8 +2,10 @@ package com.mytests.spring5gurujokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5GuruJokeAppApplication {
 
     public static void main(String[] args) {
